84f690cb4835e94be535b709f9c8677903f250c4
bifot <bifot@bifot.ru>
2019-07-16 13:22:56 +0300

    improved client.ask; updated docs

--- tests/index.test.js
+++ tests/index.test.js
@@ -3,12 +3,15 @@ const client = require('../examples/client');
 const server = require('../examples/server');
 
 it('should get response', async () => {
-  const response = await client.ask('users.get', {
+  const user = await client.ask('users.get', {
     age: 16,
   });
 
   expect(response).to.be.deep.equal({
-    name: 'Mikhail Semin',
-    age: 16,
+    error: undefined,
+    response: {
+      name: 'Mikhail Semin',
+      age: 16,
+    },
   });
 });
