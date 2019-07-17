5f1c26e421518328c2a5c859c1579b19244dc8b6
bifot <bifot@bifot.ru>
2019-07-17 12:28:41 +0300

    updated examples

--- examples/server.js
+++ examples/server.js
@@ -3,6 +3,15 @@ const { Server } = require('../src');
 const server = new Server();
 
 server.on((ctx) => {
+  if (ctx.params.age < 18) {
+    ctx.error = {
+      code: 400,
+      message: 'Invalid age',
+    };
+
+    return;
+  }
+
   ctx.body = {
     name: 'Mikhail Semin',
     age: ctx.params.age,
